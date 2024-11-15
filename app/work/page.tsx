77e4226680f09f361423e83fd9b2e30ef0f371a7
PhelipeG <luisphelipe1000@gmail.com>
2024-11-15 13:34:53 -0300

    added projets section

---
+++ app/work/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { motion } from "framer-motion";
+import { projectsData } from "../../data/projects-data";
+import Link from "next/link";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { BsGithub } from "react-icons/bs";
+
+export default function Work() {
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 1, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          {projectsData.map((project) => (
+            <div
+              key={project.id}
+              className="bg-gray-800 p-6 rounded-lg shadow-lg"
+            >
+              <img
+                src={project.image}
+                alt={project.title}
+                className=" object-cover rounded-lg mb-4"
+              />
+              <h2 className="text-2xl font-bold text-white mb-2">
+                {project.title}
+              </h2>
+              <p className="text-white/60 mb-4">{project.description}</p>
+              <ul className="flex flex-wrap gap-2 mb-4">
+                {project.stack &&
+                  project.stack.map((item, index) => (
+                    <li
+                      key={index}
+                      className="text-sm text-accent bg-gray-700 px-2 py-1 rounded"
+                    >
+                      {item.name}
+                    </li>
+                  ))}
+              </ul>
+              <div className="flex items-center gap-4">
+                <Link href={project.link} className="">
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Repositório no Github</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </motion.section>
+  );
+}
